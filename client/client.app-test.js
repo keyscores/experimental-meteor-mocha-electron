20ada92faacb84dcfb33ba361f3be3536ace5aa2
Lucas Geiger <LUCAS@Lucass-MacBook-Air.local>
2017-02-19 18:07:59 -0500

    cleanup style

--- client/client.app-test.js
+++ client/client.app-test.js
@@ -1,11 +1,11 @@
 /* eslint-env mocha */
 
 if (Meteor.isClient) {
-  describe('Client test', () => {
-    it('simple test', () => {
-      throw 'test'
-    })
-  })
+  // describe('Client test', () => {
+  //   it('simple test', () => {
+  //     throw 'test'
+  //   })
+  // })
   describe('Client test', () => {
     it('another test', () => {
     })
